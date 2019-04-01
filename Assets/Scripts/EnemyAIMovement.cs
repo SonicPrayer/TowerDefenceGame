2a593dd3a229882e6249c8f259cd9a66d4dd422c
SonicPrayer <joykariot@gmail.com>
2019-04-01 23:49:06 +0300

    Init

---
+++ Assets/Scripts/EnemyAIMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIMovement : MonoBehaviour
+{
+    public float enemySpeed = 5f; // Скорость врага
+
+    private Transform target; // Положение врага
+    private int wavepointIndex = 0; // Начальная позиция
+
+    private void Start()
+    {
+        target = Waypoints.wayPoints[0]; // Даем проложению врага трансформ первой точки из массива всех точек
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemySpeed * Time.deltaTime, Space.World); // Идем дальше
+
+        // Пока не достигнем следующей точки
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint(); // Идем в следующую точку
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        // Если это последняя точка
+        if (wavepointIndex >= Waypoints.wayPoints.Length - 1)
+        {
+            Destroy(gameObject); // Уничтожить врага
+            return;
+        }
+
+        wavepointIndex++; // Берем идекс следующей точки
+        target = Waypoints.wayPoints[wavepointIndex]; // Идем в эту точку
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] wayPoints; // Массив трансформов точек, по которым идет враг
+
+    private void Awake()
+    {
+        wayPoints = new Transform[transform.childCount]; // Создаем новый массив с числом int кол-ва чайлдов в объекте
+        // Проходим по массиву точек
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPoints[i] = transform.GetChild(i); // Запушиваем их в массив
+        }
+    }
+}
